57490bac94cb0a6d245be27626baeb5907bb5810
Brayan <Brayan@PC_Brayan>
2025-07-09 19:30:05 -0300

    Subindo as atualizações do projeto

--- loja_virtual_mentoria/src/main/java/jdev/mentoria/lojavirtual/controller/AcessoController.java
+++ loja_virtual_mentoria/src/main/java/jdev/mentoria/lojavirtual/controller/AcessoController.java
@@ -1,11 +1,13 @@
 package jdev.mentoria.lojavirtual.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -44,6 +46,35 @@ public class AcessoController {
 		}
 		
 		return ResponseEntity.noContent().build();
+	}
+	
+	@DeleteMapping
+	public ResponseEntity<?> deletar(@RequestBody Acesso acesso){
+		Optional<Acesso> acessoFiltrado = acessoRepository.findById(acesso.getId());
+		
+		if (acessoFiltrado.isPresent()) {
+			acessoService.deletar(acesso);
+			return ResponseEntity.noContent().build();
+		}
+		
+		return ResponseEntity.notFound().build();
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Acesso> buscarPorId(@PathVariable Long id){
+		Optional<Acesso> acessoFiltrado = acessoRepository.findById(id);
+		
+		if (acessoFiltrado.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+		
+		return ResponseEntity.ok(acessoFiltrado.get());
+	}
+	
+	@GetMapping("/{descricao}")
+	public ResponseEntity<List<Acesso>> buscarPorDescricao(@PathVariable String descricao){
+		List<Acesso> acessosFiltrados = acessoRepository.buscarAcessoDescricao(descricao);
+		return ResponseEntity.ok(acessosFiltrados);
 	}
 
 }
---
+++ loja_virtual_mentoria/src/main/resources/db/migration/V002__alterar_tabela_pessoa.sql
@@ -0,0 +1,3 @@
+alter table pessoa_fisica add column tipo_pessoa varchar(20);
+
+alter table pessoa_juridica add column tipo_pessoa varchar(20);
